ef01565f862631de0f494e2091f6bf92e80b4672
Tattvavabodha <nandkulkarni16@gmail.com>
2026-06-28 19:33:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("charaka-card")
+.addEventListener("click", function(){
+
+alert("Caraka-Saṃhitā section coming soon");
+
+});
+
+document.getElementById("sushruta-card")
+.addEventListener("click", function(){
+
+alert("Suśruta-Saṃhitā section coming soon");
+
+});
+
+document.getElementById("ashtanga-card")
+.addEventListener("click", function(){
+
+window.location.href = "ah/sutrasthana.html";
+
+});
